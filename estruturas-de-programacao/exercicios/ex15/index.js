ac0bfa3807c56b56aec3abf4c2c398332fe0fcf5
LucasDuarteV <lucasdv2733@gmail.com>
2026-07-16 11:29:32 -0300

    Exercício 15: solução

---
+++ estruturas-de-programacao/exercicios/ex15/index.js
@@ -0,0 +1,19 @@
+let numero = 20;
+let primo = true;
+
+if (numero <= 1) {
+    primo = false;
+} else {
+    for (let i = 2; i < numero; i++) {
+        if (numero % i === 0) {
+            primo = false;
+            break;
+        }
+    }
+}
+
+if (primo) {
+    console.log(`${numero} é um número primo.`);
+} else {
+    console.log(`${numero} não é um número primo.`);
+}
