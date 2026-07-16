04cf23b63443db2ec63b95e3d180700b597b5c08
LucasDuarteV <lucasdv2733@gmail.com>
2026-07-16 12:56:06 -0300

    Definindo uma função: prática

---
+++ funcoes/definindo-uma-funcao/index.js
@@ -0,0 +1,17 @@
+function imprimirNoConsole(){
+    console.log('Olá, Mundo!')
+}
+
+imprimirNoConsole()
+
+function imprimirUmNumero(num){
+    console.log('O número é: ' + num)
+}
+
+imprimirUmNumero(5)
+
+const numeroAleatorio = function(){
+    console.log(Math.random())
+}
+
+numeroAleatorio()
