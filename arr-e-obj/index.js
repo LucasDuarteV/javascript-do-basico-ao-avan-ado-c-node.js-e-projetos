2a0c810c85279bb9f995421d8ff8d0501f13c89f
LucasDuarteV <lucasdv2733@gmail.com>
2026-07-17 13:32:14 -0300

    Mais sobre arrays

--- arr-e-obj/index.js
+++ arr-e-obj/index.js
@@ -3,7 +3,7 @@ let nomes = ['Lucas','Luan']
 let bool = [true,false]
 let misturado = ['Lucas', 22 , true]
 
-console.log(arr)
-console.log(nomes)
-console.log(bool)
-console.log(misturado)
+console.log(arr[2])
+console.log(nomes[1])
+console.log(bool[1])
+console.log(misturado[0])
