fcac78c6b3946c8fff0ff2f97454745ee2020a30
LucasDuarteV <lucasdv2733@gmail.com>
2026-07-17 14:19:47 -0300

    O que são objetos

--- arr-e-obj/index.js
+++ arr-e-obj/index.js
@@ -1,9 +1,7 @@
-let arr = [1,2,3,4,5]
-let nomes = ['Lucas','Luan']
-let bool = [true,false]
-let misturado = ['Lucas', 22 , true]
+let cachorro ={
+    patas: 4,
+    nome: "Ted"
+}
 
-console.log(arr[2])
-console.log(nomes[1])
-console.log(bool[1])
-console.log(misturado[0])
+console.log(cachorro.nome)
+console.log(cachorro.patas)
