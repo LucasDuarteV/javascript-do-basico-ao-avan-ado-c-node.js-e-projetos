863edab7fe259767f84c3b0d0b507c5782d768bc
LucasDuarteV <lucasdv2733@gmail.com>
2026-07-17 13:30:45 -0300

    O que são arrays

---
+++ arr-e-obj/index.js
@@ -0,0 +1,9 @@
+let arr = [1,2,3,4,5]
+let nomes = ['Lucas','Luan']
+let bool = [true,false]
+let misturado = ['Lucas', 22 , true]
+
+console.log(arr)
+console.log(nomes)
+console.log(bool)
+console.log(misturado)
