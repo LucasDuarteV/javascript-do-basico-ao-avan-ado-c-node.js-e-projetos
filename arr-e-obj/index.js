48ff6856536e42a391c5c1c3e2fd7b8d1f0c2b6c
LucasDuarteV <lucasdv2733@gmail.com>
2026-07-20 10:41:51 -0300

    Rest operator

--- arr-e-obj/index.js
+++ arr-e-obj/index.js
@@ -1,3 +1,16 @@
-let palavra = 'Repetir'
+let num = 1
+let num1 = 5
+let num2 = 3
+let num3 = 4
 
-console.log(palavra.repeat(20))
+function imprimirNumeros(...args){
+    for (let i = 0; i < args.length; i++){
+        console.log(args[i])
+    }
+}
+
+imprimirNumeros(num,num1,num2)
+console.log('Pausa')
+imprimirNumeros(num2,num3)
+
+imprimirNumeros(2,6,7,8,9)
