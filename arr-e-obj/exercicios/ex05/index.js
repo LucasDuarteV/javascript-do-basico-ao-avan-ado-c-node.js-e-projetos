892d122587401065100738af9c56862003037ed0
LucasDuarteV <lucasdv2733@gmail.com>
2026-07-20 13:02:07 -0300

    Exercício 30: solução

---
+++ arr-e-obj/exercicios/ex05/index.js
@@ -0,0 +1,7 @@
+let arr = ["Valdira" , "Lucas" , "Luan" , "Josiane" , "Edson"]
+
+if(arr.includes('Lucas')){
+    console.log('O nome foi encontrado!')
+}else{
+    console.log('O nome não foi encontrado!')
+}
