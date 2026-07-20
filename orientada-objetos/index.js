a9d9b37e053bb84d47d158cb93a1a7ff71395bfb
LucasDuarteV <lucasdv2733@gmail.com>
2026-07-20 14:53:01 -0300

    Um pouco mais sobre métodos

--- orientada-objetos/index.js
+++ orientada-objetos/index.js
@@ -1,11 +1,18 @@
 const cachorro = {
+    raca: 'SRD',
     uivar: function(){
         console.log('Auuuuuuuuuuu!')
     },
     rosnar: function(){
         console.log('Grrrrrrrrrrrrrr!')
+    },
+    setRaca(raca){
+        this.raca = raca
+    },
+    getRaca(){
+        return console.log('A raça do meu cachorro é: ' + this.raca)
     }
 }
 
-cachorro.uivar()
-cachorro.rosnar()
+cachorro.setRaca('Pastor alemão')
+cachorro.getRaca()
