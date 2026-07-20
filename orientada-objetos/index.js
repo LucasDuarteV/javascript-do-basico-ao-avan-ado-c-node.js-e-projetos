788cc733c30b628279de11e1ac58a464fd464de4
LucasDuarteV <lucasdv2733@gmail.com>
2026-07-20 15:16:44 -0300

    Instanciando classes por função

--- orientada-objetos/index.js
+++ orientada-objetos/index.js
@@ -1,15 +1,10 @@
-let cachorro = {
-    patas: 4,
-    raca: "SRD",
-    latir: function(){
-        console.log('Au au')
-    }
+function criarCachorro(raca,patas,cor){
+    let cachorro = Object.create({})
+    cachorro.raca= raca,
+    cachorro.patas = patas,
+    cachorro.cor = cor
+    return cachorro
 }
 
-let labrador = Object.create(cachorro)
-
-labrador.latir()
-
-labrador.raca = 'Labradror'
-
-console.log(labrador)
+let doberman = criarCachorro('Dorberman',4,'Branco')
+console.log(doberman)
