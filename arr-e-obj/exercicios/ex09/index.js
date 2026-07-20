e9fb91b752ad65252b7fb0f510801805dfa05a25
LucasDuarteV <lucasdv2733@gmail.com>
2026-07-20 14:34:30 -0300

    Exercício 35: solução

---
+++ arr-e-obj/exercicios/ex09/index.js
@@ -0,0 +1,11 @@
+let calculadora = {
+    "somar":  (a , b) => a + b,
+    "subtrair": (a , b) => a - b,
+    "multiplicar": (a , b) => a * b,
+    "dividir":  (a , b) => a / b
+}
+
+console.log(calculadora.somar(12,2))
+console.log(calculadora.subtrair(5,2))
+console.log(calculadora.multiplicar(2,2))
+console.log(calculadora.dividir(10,2))
