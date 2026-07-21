233e62f601188f9e6e95ef723a5dcc6882b51458
LucasDuarteV <lucasdv2733@gmail.com>
2026-07-21 15:08:25 -0300

    Tratamento de input por função

--- debugs-e-erros/index.js
+++ debugs-e-erros/index.js
@@ -1,3 +1,11 @@
-"use strict"
+function checarNumero(num){
+    let number = Number(num)
+    if(Number.isNaN(number)){
+        console.log("Por favor passe um número!")
+    } else{
+        return number
+    }
+}
 
-let opa = "teste"
+checarNumero(5)
+checarNumero('Lu')
