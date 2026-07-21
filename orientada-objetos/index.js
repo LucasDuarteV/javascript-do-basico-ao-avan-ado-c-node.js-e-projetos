9bc93e1973a57b2619e0b2dacb4c9b8899ca6700
LucasDuarteV <lucasdv2733@gmail.com>
2026-07-21 09:24:43 -0300

    Class e constructor do ES6

--- orientada-objetos/index.js
+++ orientada-objetos/index.js
@@ -1,19 +1,10 @@
-function Cachorro(raca,patas,cor){
-    this.raca = raca
-    this.patas = patas
-    this.cor = cor
+class Cachorro{
+    constructor(raca,patas,cor){
+        this.raca = raca
+        this.patas = patas
+        this.cor =  cor
+    }
 }
 
-Cachorro.prototype.uivar = function(){
-    console.log("Auuuuuuuu")
-}
-
-Cachorro.prototype.latir = function(){
-    console.log("Au au")
-}
-
-let husky = new Cachorro('Husky',4,'cinza')
-
-console.log(husky)
-husky.uivar()
-husky.latir()
+let labrador = new Cachorro('Labrador',4,'Amarelo')
+console.log(labrador)
