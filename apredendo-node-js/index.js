c33089c60d1fde02a380d27a721d1487eaf07474
LucasDuarteV <lucasdv2733@gmail.com>
2026-07-23 10:59:41 -0300

    Criando rota teste

--- apredendo-node-js/index.js
+++ apredendo-node-js/index.js
@@ -6,6 +6,10 @@ app.get('/', (req,res) =>{
     res.send('Primeira rota criada!')
 })
 
+app.get('/teste' , (req , res) =>{
+    res.send('Testando rota!')
+})
+
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000!')
 })
