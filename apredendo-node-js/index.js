e9136b44168d42df025ca573d1d143559dfb89a0
LucasDuarteV <lucasdv2733@gmail.com>
2026-07-23 09:15:57 -0300

    Escrevendo em arquivos com Node

--- apredendo-node-js/index.js
+++ apredendo-node-js/index.js
@@ -7,3 +7,13 @@ fs.readFile('arquivo.txt' , 'utf8' , (erro,texto) =>{
         console.log(texto)
     }
 })
+
+const { writeFile } = require('fs')
+
+writeFile('arquivo.txt' , 'Olá, Mundo!' , (erro => {
+    if(erro){
+        throw erro
+    } else{
+        console.log('Arquivo criado com sucesso!')
+    }
+}))
